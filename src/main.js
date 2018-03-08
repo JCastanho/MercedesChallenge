746ef2637a57b23e897f6825f227fb477524567f
JCastanho <joao_cast@hotmail.com>
2018-03-08 01:16:42 +0000

    clean code

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
 var counter = 0;
 var total = 0;
 
-/*function total() {
-    for (var i = 0; i < 2; i++) {
-        var q = document.getElementById("Q"+i).innerHTML;
-        var p = document.getElementById("P"+i).innerHTML;
-        total = parseInt(q) * parseInt(p)
-    }
-    document.getElementById("TotalV").innerHTML = total;
-}*/
-
 function plusQ(x){
     var oldValue = document.getElementById("Q"+x).innerHTML;
     var value = parseInt(oldValue) + 1;
@@ -17,10 +8,7 @@ function plusQ(x){
 
     var total = document.getElementById("TotalV").innerHTML;
     var p = document.getElementById("P"+x).innerHTML;
-    console.log(document.getElementById("P"+x).innerHTML);
     total = parseFloat(document.getElementById("TotalV").innerHTML.replace(/,/, '.')) + parseFloat(p.replace(/,/, '.'));
-    console.log(document.getElementById("TotalV").innerHTML);
-    console.log(total);
     document.getElementById("TotalV").innerHTML = total.toFixed(2) + ' €';
 }
 
